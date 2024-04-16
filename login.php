e5427fbbc9eb3130ceb4e135901647ebf6274f4e
Mon82sj <115721479+Mon82sj@users.noreply.github.com>
2024-04-16 14:49:33 +0530

    login.php

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
-// Create connection
 $conn = mysqli_connect('localhost', 'root','', 'bank');
-
 if($conn){
     echo "connected";
 }
@@ -13,16 +11,11 @@ $accountNo = $_POST['accountNo'];
 $name = $_POST['name'];
 $mobile = $_POST['mobile'];
 $balance = $_POST['balance'];
-
-// Insert data into the database
 $sql = "INSERT INTO banking (Account_no, Name, Mobile, Balance) VALUES ('$accountNo', '$name', '$mobile', '$balance')";
-
 if ($conn->query($sql) === TRUE) {
     echo "Record added successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
-// Close the database connection
 $conn->close();
 ?>
